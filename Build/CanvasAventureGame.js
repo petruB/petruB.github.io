2d766904d286f4b729b1550b393c4514520c6b1a
petrubr <petryx_x@yahoo.co.uk>
2021-04-12 11:02:44 +0200

    hfgh

---
+++ Build/CanvasAventureGame.js
@@ -0,0 +1,16 @@
+{
+"companyName": "480Hz",
+"productName": "AndroidAventureGame",
+"productVersion": "1.0",
+"dataUrl": "CanvasAventureGame.data.unityweb",
+"wasmCodeUrl": "CanvasAventureGame.wasm.code.unityweb",
+"wasmFrameworkUrl": "CanvasAventureGame.wasm.framework.unityweb",
+"graphicsAPI": ["WebGL 1.0"],
+"webglContextAttributes": {"preserveDrawingBuffer": false},
+"splashScreenStyle": "Dark",
+"backgroundColor": "#231F20",
+"cacheControl": {"default": "must-revalidate"},
+"developmentBuild": false,
+"multithreading": false,
+"unityVersion": "2019.4.18f1"
+}
