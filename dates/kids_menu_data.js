e192e95c9cc3261e84df78ed4c3d019aa7083621
petrubr <petryx_x@yahoo.co.uk>
2023-05-17 11:08:38 +0200

    ryy
    ry

--- dates/kids_menu_data.js
+++
@@ -1,9 +0,0 @@
-export default {
-  kids: {
-    screenIndex:3,
-    entertainButton: "images/kids_movie_button.png",
-    entertainRouterPath: "/KidsList",
-    gamesButton:     "images/kids_games_button.png",
-    gamesRouterPath: "/KidsGames",
-  }
-};
